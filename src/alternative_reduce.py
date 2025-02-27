ff9ab2d063d102182bd321e7440fd30c107bdd37
annikagaglani <agaglan@students.pitzer.edu>
2025-02-26 21:33:36 -0800

    updated hashtag trends

--- src/alternative_reduce.py
+++ src/alternative_reduce.py
@@ -50,9 +50,6 @@ for hashtag, counts in hashtag_counts.items():
 plt.xlabel("Date")
 plt.ylabel("Number of Tweets")
 plt.title("Hashtag Trends Over Time")
-plt.xticks(rotation=45, fontsize=8)
-
-#Print label only every 20 dates
 plt.xticks(dates[::20], rotation=45, fontsize=8)
 
 plt.legend()
